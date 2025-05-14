5bc5b6eefaede72218f50a2fbc0157073403ba69
hwoo <tone8943@gmail.com>
2025-05-14 21:08:07 +0900

    Initial commit

---
+++ src/main/java/com/hwoo/photogram/PhotogramApplication.java
@@ -0,0 +1,13 @@
+package com.hwoo.photogram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotogramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotogramApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hwoo/photogram/PhotogramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hwoo.photogram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotogramApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
