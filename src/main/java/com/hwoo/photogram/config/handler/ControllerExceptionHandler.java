cd82af29a8239f1a702f6f7e9566c5de23c7955e
hwoo <tone8943@gmail.com>
2025-05-16 21:02:29 +0900

    [AUTH] 시큐리티 - 로그인 기초 - 예외처리에서 SCRIPT로 응답

--- src/main/java/com/hwoo/photogram/config/handler/ControllerExceptionHandler.java
+++ src/main/java/com/hwoo/photogram/config/handler/ControllerExceptionHandler.java
@@ -1,22 +1,20 @@
 package com.hwoo.photogram.config.handler;
 
 import com.hwoo.photogram.config.handler.ex.CustomValidationException;
-import com.hwoo.photogram.web.enums.CommonResponseCode;
-import com.hwoo.photogram.web.exception.CommonResponse;
+import com.hwoo.photogram.util.Script;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-import java.util.Map;
 
 @RestControllerAdvice
 public class ControllerExceptionHandler {
 
     @ExceptionHandler(CustomValidationException.class)
-    public CommonResponse<?> validationException(CustomValidationException e) {
-        return CommonResponse.<Map<String, String>>builder()
-                .code(CommonResponseCode.VALIDATION_FAIL.getCode())
-                .message(e.getMessage())
-                .data(e.getErrorMap())
-                .build();
+    public String validationException(CustomValidationException e) {
+//        return CommonResponse.<Map<String, String>>builder()
+//                .code(CommonResponseCode.VALIDATION_FAIL.getCode())
+//                .message(e.getMessage())
+//                .data(e.getErrorMap())
+//                .build();
+        return Script.back(e.getErrorMap().toString());
     }
 }
